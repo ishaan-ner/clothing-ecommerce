b0afbb0f5daa011cb3a4f6673af1633a30292c9d
Ishaan Nerurkar <ishaan.ner@gmail.com>
2020-07-17 22:26:19 +1000

    Updated action types to be variables instead of strings for best practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/Homepage";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Shop from "./pages/shop/Shop";
 import Header from "./components/header/Header";
 import SignIn from "./pages/sign-in/SignIn";
@@ -9,7 +9,7 @@ import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
 
-function App({ setCurrentUser }) {
+function App({ setCurrentUser, currentUser }) {
   useEffect(() => {
     const unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
       if (user) {
@@ -22,7 +22,7 @@ function App({ setCurrentUser }) {
       setCurrentUser(user);
     });
     return unsubscribeFromAuth;
-  });
+  }, [setCurrentUser]);
 
   return (
     <div>
@@ -30,14 +30,22 @@ function App({ setCurrentUser }) {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={Shop} />
-        <Route exact path="/signin" component={SignIn} />
+        <Route
+          exact
+          path="/signin"
+          render={() => (currentUser ? <Redirect to="/" /> : <SignIn />)}
+        />
       </Switch>
     </div>
   );
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);

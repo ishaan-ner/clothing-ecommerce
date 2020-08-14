d749df976caba133d1863b5cc675e33373f96683
Ishaan Nerurkar <ishaan.ner@gmail.com>
2020-08-14 16:37:26 +1000

    added lazy loading and suspense to site pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from "react";
-import "./App.scss";
-import HomePage from "./pages/homepage/Homepage";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Shop from "./pages/shop/Shop";
-import Header from "./components/header/Header";
-import SignIn from "./pages/sign-in/SignIn";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
-import Checkout from "./pages/checkout/Checkout";
+import "./App.scss";
+import HomePage from "./pages/homepage/Homepage";
+import Header from "./components/header/Header";
+
+const Shop = lazy(() => import("./pages/shop/Shop"));
+const SignIn = lazy(() => import("./pages/sign-in/SignIn"));
+const Checkout = lazy(() => import("./pages/checkout/Checkout"));
 
 function App({ setCurrentUser, currentUser }) {
   useEffect(() => {
@@ -32,13 +33,15 @@ function App({ setCurrentUser, currentUser }) {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={Shop} />
-        <Route
-          exact
-          path="/signin"
-          render={() => (currentUser ? <Redirect to="/" /> : <SignIn />)}
-        />
-        <Route exact path="/checkout" component={Checkout} />
+        <Suspense fallback={<div>Loading</div>}>
+          <Route path="/shop" component={Shop} />
+          <Route
+            exact
+            path="/signin"
+            render={() => (currentUser ? <Redirect to="/" /> : <SignIn />)}
+          />
+          <Route exact path="/checkout" component={Checkout} />
+        </Suspense>
       </Switch>
     </div>
   );
